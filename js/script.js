868c69ab0a100ac2c85049915c7c062e7191e89c
Heetarthi Kamdar <heetarthikamdar@gmail.com>
2020-01-18 18:44:38 +0530

    admin new pages

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-/*
-
-Style   : MobApp Script JS
-Version : 1.0
-Author  : Surjith S M
-URI     : https://surjithctly.in/
-
-Copyright © All rights Reserved 
-
-*/
-
 $(function() {
     "use strict";
 
@@ -113,112 +102,111 @@ $(function() {
 
 
 
-    // vars
-    'use strict'
-    var testim = document.getElementById("testim"),
-        testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
-        testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
-        testimLeftArrow = document.getElementById("left-arrow"),
-        testimRightArrow = document.getElementById("right-arrow"),
-        testimSpeed = 4500,
-        currentSlide = 0,
-        currentActive = 0,
-        testimTimer,
-        touchStartPos,
-        touchEndPos,
-        touchPosDiff,
-        ignoreTouch = 30;
-    ;
+// vars
+'use strict'
+var testim = document.getElementById("testim"),
+    testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+    touchStartPos,
+    touchEndPos,
+    touchPosDiff,
+    ignoreTouch = 30;;
 
-    window.onload = function () {
+window.onload = function() {
 
-        // Testim Script
-        function playSlide(slide) {
-            for (var k = 0; k < testimDots.length; k++) {
-                testimContent[k].classList.remove("active");
-                testimContent[k].classList.remove("inactive");
-                testimDots[k].classList.remove("active");
-            }
-
-            if (slide < 0) {
-                slide = currentSlide = testimContent.length - 1;
-            }
-
-            if (slide > testimContent.length - 1) {
-                slide = currentSlide = 0;
-            }
-
-            if (currentActive != currentSlide) {
-                testimContent[currentActive].classList.add("inactive");
-            }
-            testimContent[slide].classList.add("active");
-            testimDots[slide].classList.add("active");
-
-            currentActive = currentSlide;
-
-            clearTimeout(testimTimer);
-            testimTimer = setTimeout(function () {
-                playSlide(currentSlide += 1);
-            }, testimSpeed)
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
         }
 
-        testimLeftArrow.addEventListener("click", function () {
-            playSlide(currentSlide -= 1);
-        })
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length - 1;
+        }
 
-        testimRightArrow.addEventListener("click", function () {
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function() {
             playSlide(currentSlide += 1);
-        })
+        }, testimSpeed)
+    }
 
-        for (var l = 0; l < testimDots.length; l++) {
-            testimDots[l].addEventListener("click", function () {
-                playSlide(currentSlide = testimDots.indexOf(this));
-            })
-        }
+    testimLeftArrow.addEventListener("click", function() {
+        playSlide(currentSlide -= 1);
+    })
 
-        playSlide(currentSlide);
+    testimRightArrow.addEventListener("click", function() {
+        playSlide(currentSlide += 1);
+    })
 
-        // keyboard shortcuts
-        document.addEventListener("keyup", function (e) {
-            switch (e.keyCode) {
-                case 37:
-                    testimLeftArrow.click();
-                    break;
-
-                case 39:
-                    testimRightArrow.click();
-                    break;
-
-                case 39:
-                    testimRightArrow.click();
-                    break;
-
-                default:
-                    break;
-            }
-        })
-
-        testim.addEventListener("touchstart", function (e) {
-            touchStartPos = e.changedTouches[0].clientX;
-        })
-
-        testim.addEventListener("touchend", function (e) {
-            touchEndPos = e.changedTouches[0].clientX;
-
-            touchPosDiff = touchStartPos - touchEndPos;
-
-            console.log(touchPosDiff);
-            console.log(touchStartPos);
-            console.log(touchEndPos);
-
-
-            if (touchPosDiff > 0 + ignoreTouch) {
-                testimLeftArrow.click();
-            } else if (touchPosDiff < 0 - ignoreTouch) {
-                testimRightArrow.click();
-            } else {
-                return;
-            }
-
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function() {
+            playSlide(currentSlide = testimDots.indexOf(this));
         })
     }
+
+    playSlide(currentSlide);
+
+    // keyboard shortcuts
+    document.addEventListener("keyup", function(e) {
+        switch (e.keyCode) {
+            case 37:
+                testimLeftArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            default:
+                break;
+        }
+    })
+
+    testim.addEventListener("touchstart", function(e) {
+        touchStartPos = e.changedTouches[0].clientX;
+    })
+
+    testim.addEventListener("touchend", function(e) {
+        touchEndPos = e.changedTouches[0].clientX;
+
+        touchPosDiff = touchStartPos - touchEndPos;
+
+        console.log(touchPosDiff);
+        console.log(touchStartPos);
+        console.log(touchEndPos);
+
+
+        if (touchPosDiff > 0 + ignoreTouch) {
+            testimLeftArrow.click();
+        } else if (touchPosDiff < 0 - ignoreTouch) {
+            testimRightArrow.click();
+        } else {
+            return;
+        }
+
+    })
+}
