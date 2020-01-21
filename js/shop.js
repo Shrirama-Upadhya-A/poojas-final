0037607195f6d32425df566664d9d24e61c34539
Heetarthi Kamdar <heetarthikamdar@gmail.com>
2020-01-21 15:34:56 +0530

    shop

--- js/shop.js
+++ js/shop.js
@@ -91,3 +91,16 @@ jQuery(document).ready(function($) {
     });
 
 });
+
+$(document).ready(function() {
+
+    // Lift card and show stats on Mouseover
+    $('#product-card').hover(function() {
+        $(this).addClass('animate');
+        $('div.carouselNext, div.carouselPrev').addClass('visible');
+    }, function() {
+        $(this).removeClass('animate');
+        $('div.carouselNext, div.carouselPrev').removeClass('visible');
+    });
+
+});
