2bf7259d62a6eb602890a5a776ec9f769df75ffc
Shrirama-Upadhya-A <shriramaupadhya1303@gmail.com>
2020-01-22 19:45:14 +0530

    products--pundits and flowers

--- js/userFlowersSubscriptions.js
+++ js/userFlowersSubscriptions.js
@@ -8,5 +8,34 @@ var fbRef = db.ref().child("Products").child("Flowers").child("Subscriptions");
     var image = snap.child("image").val();
     // var code = snap.child("code").val();
     var number = snap.key;
-    document.getElementById("columns").innerHTML += "<figure><img src="+image+"><figcaption>"+name+"</figcaption><span class='price'>&#8377;"+price+"/Day</span><button class='button' onClick='flowerDailySubscription(\""+number+"\")'>Add To Cart</button></figure>";
-    });
+    document.getElementById("lol").innerHTML +=  `<div class="product">
+    <div class="make3D">
+        <div class="product-front">
+            <div class="shadow"></div>
+            <img src="`+image+`" alt="" />
+            <div class="image_overlay"></div>
+            <div class="add_to_cart">Add to cart</div>
+                <div class="view_details2" onClick='flowerSubscription("`+number+`")'>View Details</div>
+            
+            <div class="stats">
+                <div class="stats-container">
+                    <span class="product_price">&#8377;`+price+`</span>
+                    <span class="product_name">`+name+`</span>
+                    
+
+                </div>
+            </div>
+        </div>
+
+        <div class="product-back">
+            <div class="shadow"></div>
+            <div class="carousel">
+                <h2>hellasdfghjkljghasdfgyhuijofrdeo</h2>
+            </div>
+            <div class="flip-back">
+                <div class="cy"></div>
+                <div class="cx"></div>
+            </div>
+        </div>
+    </div>
+</div> `    });
--- js/userPundits.js
+++ js/userPundits.js
@@ -5,10 +5,40 @@ var name = snap.child("name").val();
 var des = snap.child("des").val();
 var price = snap.child("price").val();
 // var description = snap.child("des").val();
-// var image = snap.child("image").val();
+var image = snap.child("image").val();
 // var code = snap.child("code").val();
 var number = snap.key;
+document.getElementById("lol3").innerHTML +=  `<div class="product">
+    <div class="make3D">
+        <div class="product-front">
+            <div class="shadow"></div>
+            <img src="`+image+`" alt="" />
+            <div class="image_overlay"></div>
+            <div class="add_to_cart">Add to cart</div>
+                <div class="view_details2" onClick='pundits("`+number+`","Normal")'>View Details</div>
+            
+            <div class="stats">
+                <div class="stats-container">
+                    <span class="product_price">&#8377;`+price+`</span>
+                    <span class="product_name">`+name+`</span>
+                   
 
+                </div>
+            </div>
+        </div>
+
+        <div class="product-back">
+            <div class="shadow"></div>
+            <div class="carousel">
+                <h2>hellasdfghjkljghasdfgyhuijofrdeo</h2>
+            </div>
+            <div class="flip-back">
+                <div class="cy"></div>
+                <div class="cx"></div>
+            </div>
+        </div>
+    </div>
+</div> `  
 // $("#NormalPandits").append("<tr><td data-th='Ref. ID'>"+number+"</td><td data-th='Product Name'>"+name+"</td><td data-th='Cost'>"+price+"</td><td data-th='View'><button type='button' style='width: 5vw;' class='btn btn-danger' onClick='flowerDailySubscriptions(\""+number+"\")'>VIEW</button></td></tr>");    
 });
 
@@ -22,10 +52,40 @@ var name = snap.child("name").val();
 var des = snap.child("des").val();
 var price = snap.child("price").val();
 // var description = snap.child("des").val();
-// var image = snap.child("image").val();
+var image = snap.child("image").val();
 // var code = snap.child("code").val();
 var number = snap.key;
+document.getElementById("lol4").innerHTML +=  `<div class="product">
+    <div class="make3D">
+        <div class="product-front">
+            <div class="shadow"></div>
+            <img src="`+image+`" alt="" />
+            <div class="image_overlay"></div>
+            <div class="add_to_cart">Add to cart</div>
+                <div class="view_details2" onClick='pundits("`+number+`","Recommended")'>View Details</div>
+            
+            <div class="stats">
+                <div class="stats-container">
+                    <span class="product_price">&#8377;`+price+`</span>
+                    <span class="product_name">`+name+`</span>
+              
 
+                </div>
+            </div>
+        </div>
+
+        <div class="product-back">
+            <div class="shadow"></div>
+            <div class="carousel">
+                <h2>hellasdfghjkljghasdfgyhuijofrdeo</h2>
+            </div>
+            <div class="flip-back">
+                <div class="cy"></div>
+                <div class="cx"></div>
+            </div>
+        </div>
+    </div>
+</div> `  
 // $("#RecommendedPandits").append("<tr><td data-th='Ref. ID'>"+number+"</td><td data-th='Product Name'>"+name+"</td><td data-th='Cost'>"+price+"</td><td data-th='View'><button type='button' style='width: 5vw;' class='btn btn-danger' onClick='flowerDailySubscriptions(\""+number+"\")'>VIEW</button></td></tr>");    
 });
 
@@ -39,9 +99,39 @@ var name = snap.child("name").val();
 var des = snap.child("des").val();
 var price = snap.child("price").val();
 // var description = snap.child("des").val();
-// var image = snap.child("image").val();
+var image = snap.child("image").val();
 // var code = snap.child("code").val();
 var number = snap.key;
+document.getElementById("lol5").innerHTML +=  `<div class="product">
+    <div class="make3D">
+        <div class="product-front">
+            <div class="shadow"></div>
+            <img src="`+image+`" alt="" />
+            <div class="image_overlay"></div>
+            <div class="add_to_cart">Add to cart</div>
+                <div class="view_details2" onClick='pundits("`+number+`","Anusthans")'>View Details</div>
+            
+            <div class="stats">
+                <div class="stats-container">
+                    <span class="product_price">&#8377;`+price+`</span>
+                    <span class="product_name">`+name+`</span>
+               
 
+                </div>
+            </div>
+        </div>
+
+        <div class="product-back">
+            <div class="shadow"></div>
+            <div class="carousel">
+                <h2>hellasdfghjkljghasdfgyhuijofrdeo</h2>
+            </div>
+            <div class="flip-back">
+                <div class="cy"></div>
+                <div class="cx"></div>
+            </div>
+        </div>
+    </div>
+</div> `  
 // $("#AnusthansPandits").append("<tr><td data-th='Ref. ID'>"+number+"</td><td data-th='Product Name'>"+name+"</td><td data-th='Cost'>"+price+"</td><td data-th='View'><button type='button' style='width: 5vw;' class='btn btn-danger' onClick='flowerDailySubscriptions(\""+number+"\")'>VIEW</button></td></tr>");    
 });
