d33627a4430dc2af02bb0af434b7cd3bcc600663
Shrirama-Upadhya-A <shriramaupadhya1303@gmail.com>
2020-01-25 12:42:10 +0530

    fixedLoginIssues and Puja Essentials but images are being displayed side by side

--- js/userPujaEssentials.js
+++ js/userPujaEssentials.js
@@ -1,31 +1,115 @@
-// var fbRef = db.ref().child("Products").child("Puja Essentials").child("Utensils");
-// // removed the storage reference
-// fbRef.on("child_added", snap => {
-//     var name = snap.child("name").val();
-//     var des = snap.child("des").val();
-//     var price = snap.child("price").val();
-//     // var description = snap.child("des").val();
-//     var image = snap.child("image").val();
-//     var code = snap.child("code").val();
-//     var number = snap.key;
-//     console.log(name);
-//
-//
-//     document.getElementById("card1").innerHTML +=  `<li class="cards_item" onclick="flowerSpecials('${number}')">
-//         <div class="card">
-//         <div class="card_image"><img src="${image}"></div>
-//         <div class="card_content">
-//         <h2 class="card_title">${name}
-//     <br> (hindi)
-//     </h2>
-//     <p class="card_text">&#8377;${price}</p>
-//         <button class="btn card_btn">Add To Cart</button>
-//
-//     </div>
-//     </div>
-//     </li>`
-//
-// });
+var fbRef = db.ref().child("Products").child("Puja Essentials").child("Brass Utensils");
+// removed the storage reference
+fbRef.on("child_added", snap => {
+    var name = snap.child("name").val();
+    var des = snap.child("des").val();
+    var price = snap.child("price").val();
+    // var description = snap.child("des").val();
+    var image = snap.child("image").val();
+    var code = snap.child("code").val();
+    var number = snap.key;
+    console.log(name);
+
+
+    document.getElementById("brass").innerHTML +=  `<li class="cards_item" onclick="pujaEssentials('${number}','Brass Utensils')">
+        <div class="card">
+        <div class="card_image"><img src="${image}"></div>
+        <div class="card_content">
+        <h2 class="card_title">${name}
+    <br> (hindi)
+    </h2>
+    <p class="card_text">&#8377;${price}</p>
+        <button class="btn card_btn">Add To Cart</button>
+
+    </div>
+    </div>
+    </li>`
+
+});
+var fbRef = db.ref().child("Products").child("Puja Essentials").child("Copper Utensils");
+// removed the storage reference
+fbRef.on("child_added", snap => {
+    var name = snap.child("name").val();
+    var des = snap.child("des").val();
+    var price = snap.child("price").val();
+    // var description = snap.child("des").val();
+    var image = snap.child("image").val();
+    var code = snap.child("code").val();
+    var number = snap.key;
+    console.log(name);
+
+
+    document.getElementById("copper").innerHTML +=  `<li class="cards_item" onclick="pujaEssentials('${number}','Copper Utensils')">
+        <div class="card">
+        <div class="card_image"><img src="${image}"></div>
+        <div class="card_content">
+        <h2 class="card_title">${name}
+    <br> (hindi)
+    </h2>
+    <p class="card_text">&#8377;${price}</p>
+        <button class="btn card_btn">Add To Cart</button>
+
+    </div>
+    </div>
+    </li>`
+
+});
+var fbRef = db.ref().child("Products").child("Puja Essentials").child("Kasa Utensils");
+// removed the storage reference
+fbRef.on("child_added", snap => {
+    var name = snap.child("name").val();
+    var des = snap.child("des").val();
+    var price = snap.child("price").val();
+    // var description = snap.child("des").val();
+    var image = snap.child("image").val();
+    var code = snap.child("code").val();
+    var number = snap.key;
+    console.log(name);
+
+
+    document.getElementById("kasa").innerHTML +=  `<li class="cards_item" onclick="pujaEssentials('${number}','Kasa Utensils')">
+        <div class="card">
+        <div class="card_image"><img src="${image}"></div>
+        <div class="card_content">
+        <h2 class="card_title">${name}
+    <br> (hindi)
+    </h2>
+    <p class="card_text">&#8377;${price}</p>
+        <button class="btn card_btn">Add To Cart</button>
+
+    </div>
+    </div>
+    </li>`
+
+});
+var fbRef = db.ref().child("Products").child("Puja Essentials").child("Silver Utensils");
+// removed the storage reference
+fbRef.on("child_added", snap => {
+    var name = snap.child("name").val();
+    var des = snap.child("des").val();
+    var price = snap.child("price").val();
+    // var description = snap.child("des").val();
+    var image = snap.child("image").val();
+    var code = snap.child("code").val();
+    var number = snap.key;
+    console.log(name);
+
+
+    document.getElementById("silver").innerHTML +=  `<li class="cards_item" onclick="pujaEssentials('${number}','Silver Utensils')">
+        <div class="card">
+        <div class="card_image"><img src="${image}"></div>
+        <div class="card_content">
+        <h2 class="card_title">${name}
+    <br> (hindi)
+    </h2>
+    <p class="card_text">&#8377;${price}</p>
+        <button class="btn card_btn">Add To Cart</button>
+
+    </div>
+    </div>
+    </li>`
+
+});
 
 var fbRef = db.ref().child("Products").child("Puja Essentials").child("Samagris");
 // removed the storage reference
@@ -56,34 +140,118 @@ fbRef.on("child_added", snap => {
 
 });
 
-// var fbRef = db.ref().child("Products").child("Puja Essentials").child("Poshak");
-// // removed the storage reference
-// fbRef.on("child_added", snap => {
-//     var name = snap.child("name").val();
-//     var des = snap.child("des").val();
-//     var price = snap.child("price").val();
-//     // var description = snap.child("des").val();
-//     var image = snap.child("image").val();
-//     var code = snap.child("code").val();
-//     var number = snap.key;
-//     console.log(name);
-//
-//
-//     document.getElementById("card3").innerHTML +=  `<li class="cards_item" onclick="pujaEssentials('${number}','Poshak')">
-//         <div class="card">
-//         <div class="card_image"><img src="${image}"></div>
-//         <div class="card_content">
-//         <h2 class="card_title">${name}
-//     <br> (hindi)
-//     </h2>
-//     <p class="card_text">&#8377;${price}</p>
-//         <button class="btn card_btn">Add To Cart</button>
-//
-//     </div>
-//     </div>
-//     </li>`
-//
-// });
+var fbRef = db.ref().child("Products").child("Puja Essentials").child("Asanas");
+// removed the storage reference
+fbRef.on("child_added", snap => {
+    var name = snap.child("name").val();
+    var des = snap.child("des").val();
+    var price = snap.child("price").val();
+    // var description = snap.child("des").val();
+    var image = snap.child("image").val();
+    var code = snap.child("code").val();
+    var number = snap.key;
+    console.log(name);
+
+
+    document.getElementById("asanas").innerHTML +=  `<li class="cards_item" onclick="pujaEssentials('${number}','Asanas')">
+        <div class="card">
+        <div class="card_image"><img src="${image}"></div>
+        <div class="card_content">
+        <h2 class="card_title">${name}
+    <br> (hindi)
+    </h2>
+    <p class="card_text">&#8377;${price}</p>
+        <button class="btn card_btn">Add To Cart</button>
+
+    </div>
+    </div>
+    </li>`
+
+});
+var fbRef = db.ref().child("Products").child("Puja Essentials").child("Chunris");
+// removed the storage reference
+fbRef.on("child_added", snap => {
+    var name = snap.child("name").val();
+    var des = snap.child("des").val();
+    var price = snap.child("price").val();
+    // var description = snap.child("des").val();
+    var image = snap.child("image").val();
+    var code = snap.child("code").val();
+    var number = snap.key;
+    console.log(name);
+
+
+    document.getElementById("chunris").innerHTML +=  `<li class="cards_item" onclick="pujaEssentials('${number}','Chunris')">
+        <div class="card">
+        <div class="card_image"><img src="${image}"></div>
+        <div class="card_content">
+        <h2 class="card_title">${name}
+    <br> (hindi)
+    </h2>
+    <p class="card_text">&#8377;${price}</p>
+        <button class="btn card_btn">Add To Cart</button>
+
+    </div>
+    </div>
+    </li>`
+
+});
+var fbRef = db.ref().child("Products").child("Puja Essentials").child("Mukuts");
+// removed the storage reference
+fbRef.on("child_added", snap => {
+    var name = snap.child("name").val();
+    var des = snap.child("des").val();
+    var price = snap.child("price").val();
+    // var description = snap.child("des").val();
+    var image = snap.child("image").val();
+    var code = snap.child("code").val();
+    var number = snap.key;
+    console.log(name);
+
+
+    document.getElementById("mukuts").innerHTML +=  `<li class="cards_item" onclick="pujaEssentials('${number}','Mukuts')">
+        <div class="card">
+        <div class="card_image"><img src="${image}"></div>
+        <div class="card_content">
+        <h2 class="card_title">${name}
+    <br> (hindi)
+    </h2>
+    <p class="card_text">&#8377;${price}</p>
+        <button class="btn card_btn">Add To Cart</button>
+
+    </div>
+    </div>
+    </li>`
+
+});
+var fbRef = db.ref().child("Products").child("Puja Essentials").child("Vastras");
+// removed the storage reference
+fbRef.on("child_added", snap => {
+    var name = snap.child("name").val();
+    var des = snap.child("des").val();
+    var price = snap.child("price").val();
+    // var description = snap.child("des").val();
+    var image = snap.child("image").val();
+    var code = snap.child("code").val();
+    var number = snap.key;
+    console.log(name);
+
+
+    document.getElementById("vastra").innerHTML +=  `<li class="cards_item" onclick="pujaEssentials('${number}','Vastras')">
+        <div class="card">
+        <div class="card_image"><img src="${image}"></div>
+        <div class="card_content">
+        <h2 class="card_title">${name}
+    <br> (hindi)
+    </h2>
+    <p class="card_text">&#8377;${price}</p>
+        <button class="btn card_btn">Add To Cart</button>
+
+    </div>
+    </div>
+    </li>`
+
+});
 
 var fbRef = db.ref().child("Products").child("Puja Essentials").child("Mandirs");
 // removed the storage reference
